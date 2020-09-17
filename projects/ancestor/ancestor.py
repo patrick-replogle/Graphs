a2d6fbd1332b61ca6dc7b4505374005d3d9dc3b4
patrick-replogle <patrickr1138@gmail.com>
2020-09-16 19:14:13 -0700

    completed working solution to ancestor and cleaned up code a bit

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -2,63 +2,65 @@ from util import Stack
 
 
 def earliest_ancestor(ancestors, starting_node):
-    # create a dict to store all nodes and their immediate neighbors
+    # create a dict to store all ancestor nodes
     ancestor_hashmap = {}
-
+    # { key -> node: value -> set of node's direct ancestors }
     for a in ancestors:
+        # add all nodes as keys in hashmap
         if a[1] not in ancestor_hashmap:
             ancestor_hashmap[a[1]] = set()
         if a[0] not in ancestor_hashmap:
             ancestor_hashmap[a[0]] = set()
-            ancestor_hashmap[a[1]].add(a[0])
-        else:
-            ancestor_hashmap[a[1]].add(a[0])
+        # then add all edges of nodes to the set
+        ancestor_hashmap[a[1]].add(a[0])
+    # find all possible paths to oldest ancestors
+    paths = find_paths_to_earliest_ancestor(starting_node, ancestor_hashmap)
 
-    paths = find_ancestors(starting_node, ancestor_hashmap)
-    print(paths)
-
-    if len(paths) == 1:
-        if paths[0][-1] == starting_node:
-            return -1
-        else:
-            return paths[0][-1]
+    # starting_node is the earliest ancestor if no valid paths
+    if len(paths) == 0:
+        return -1
+    # if only one valid path, return the oldest ancestor
+    elif len(paths) == 1:
+        return paths[0][-1]
 
     elif len(paths) == 2:
+        # longer path of the two will be oldest
         if len(paths[0]) < len(paths[1]):
             return paths[-1][-1]
+        # if path lengths are equal, return the smallest value
         elif len(paths[0]) == len(paths[1]):
             if paths[0][-1] < paths[1][-1]:
                 return paths[0][-1]
             return paths[1][-1]
-
-    elif len(paths) > 2:
+    # last value of last path array will be the oldest
+    else:
         return paths[-1][-1]
 
 
-def find_ancestors(starting_vertex, dict):
-    s = Stack()
-    s.push([starting_vertex])
+# Depth First Traversal approach to find oldest ancestor paths
+def find_paths_to_earliest_ancestor(starting_node, ancestor_hashmap):
     results = []
-
     visited = set()
+    s = Stack()
+
+    s.push([starting_node])
 
     while s.size() > 0:
-
         curr_path = s.pop()
+        curr_node = curr_path[len(curr_path) - 1]
 
-        curr_vertex = curr_path[len(curr_path) - 1]
-
-        if curr_vertex not in visited:
-            if len(dict[curr_vertex]) == 0:
+        if curr_node not in visited:
+            # We have a valid path if curr_node has no ancestors
+            if len(ancestor_hashmap[curr_node]) == 0 and curr_node != starting_node:
                 results.append(curr_path)
 
-            visited.add(curr_vertex)
+            visited.add(curr_node)
 
-            for neighbor in dict[curr_vertex]:
-                arr = curr_path[:]
-                arr.append(neighbor)
-                s.push(arr)
-
+            for neighbor in ancestor_hashmap[curr_node]:
+                path_copy = list(curr_path)
+                path_copy.append(neighbor)
+                s.push(path_copy)
+    # return all valid paths
     return results
 
 
@@ -66,7 +68,7 @@ ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7),
              (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
 
 
-print(earliest_ancestor(ancestors, 6))
+print(earliest_ancestor(ancestors, 8))
 
 # self.assertEqual(earliest_ancestor(test_ancestors, 1), 10)
 # self.assertEqual(earliest_ancestor(test_ancestors, 2), -1)
