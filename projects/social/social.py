7f7d2913ee57bef13386b48036cd06476e40003a
patrick-replogle <patrickr1138@gmail.com>
2020-09-17 15:35:58 -0700

    Added second populate graph function

--- projects/social/social.py
+++ projects/social/social.py
@@ -22,8 +22,8 @@ class User:
     def __init__(self, name):
         self.name = name
 
-    def __str__(self):
-        return f"{self.name}"
+    def __repr__(self):
+        return f"<User: {self.name}>"
 
 
 class SocialGraph:
@@ -56,6 +56,11 @@ class SocialGraph:
         if user_id in self.friendships:
             return self.friendships[user_id]
 
+    def reset(self):
+        self.last_id = 0
+        self.users = {}
+        self.friendships = {}
+
     def populate_graph(self, num_users, avg_friendships):
         """
         Takes a number of users and an average number of friendships
@@ -67,9 +72,7 @@ class SocialGraph:
         The number of users must be greater than the average number of friendships.
         """
         # Reset graph
-        self.last_id = 0
-        self.users = {}
-        self.friendships = {}
+        self.reset()
         # random names to populate network with
         first_names = ["John", "Frank", "George", "Jane", "Bruce", "Patrick", "James",
                        "Superman", "Betty", "Wilma", "Michael", "Barney", "Spiderman"]
@@ -87,11 +90,42 @@ class SocialGraph:
 
             # Create friendships
             for user_id in self.users:
-                while len(self.friendships[user_id]) < 2:
+                # repeat until user has at least avg_friendships
+                while len(self.friendships[user_id]) < avg_friendships:
                     random_id = random.randint(1, num_users)
 
                     if random_id not in self.friendships[user_id] and user_id != random_id:
                         self.add_friendship(user_id, random_id)
+        else:
+            print("Number of users must be larger that number of friendship links")
+
+    def populate_graph2(self, num_users, avg_friendships):
+        """
+        Takes a number of users and an average number of friendships
+        as arguments
+
+        Creates that number of users and a randomly distributed friendships
+        between those users.
+
+        The number of users must be greater than the average number of friendships.
+        """
+        # Reset graph
+        self.reset()
+        if num_users > avg_friendships:
+
+            for i in range(num_users):
+                self.add_user(f"User {i}")
+
+            possible_friendships = []
+
+            for user_id in self.users:
+                for friend_id in range(user_id + 1, self.last_id + 1):
+                    possible_friendships.append((user_id, friend_id))
+
+            for i in range(num_users * avg_friendships // 2):
+                friendships = possible_friendships[i]
+                self.add_friendship(friendships[0], friendships[1])
+
         else:
             print("Number of users must be larger that number of friendship links")
 
@@ -105,33 +139,30 @@ class SocialGraph:
         The key is the friend's ID and the value is the path.
         """
         if len(self.friendships) > 0:
-            # Store all network connection paths in a dictionary
             # {key -> friend_id, value -> [shortest friendship path between friend and user_id]}
             results = {}
             # Iterate thru social network
             for user in self.users:
-                # Ignore the user that was passed into the function
-                if user != user_id:
-                    # BFS to find shortest path of connections to get back to the user_id
-                    q = Queue()
-                    visited = set()
+                # BFS to find shortest path of connections to get from user_id to current_user in loop
+                q = Queue()
+                visited = set()
 
-                    q.enqueue([user])
+                q.enqueue([user_id])
 
-                    while q.size() > 0:
-                        curr_path = q.dequeue()
-                        curr_user = curr_path[-1]
+                while q.size() > 0:
+                    curr_path = q.dequeue()
+                    curr_user = curr_path[-1]
 
-                        if curr_user not in visited:
-                            if curr_user == user_id:
-                                results[user] = curr_path
+                    if curr_user not in visited:
+                        if curr_user == user:
+                            results[user] = curr_path
 
-                            visited.add(curr_user)
+                        visited.add(curr_user)
 
-                            for friend in self.get_friendships(curr_user):
-                                path_copy = curr_path[:]
-                                path_copy.append(friend)
-                                q.enqueue(path_copy)
+                        for friend in self.get_friendships(curr_user):
+                            path_copy = curr_path[:]
+                            path_copy.append(friend)
+                            q.enqueue(path_copy)
 
             return results
 
