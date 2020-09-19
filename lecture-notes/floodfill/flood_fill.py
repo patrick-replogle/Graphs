b6f35883c1de3b453738a5eeb1d5ad29c18f70c4
patrick-replogle <patrickr1138@gmail.com>
2020-09-19 11:56:57 -0700

    Added stack representation to floodfill

--- lecture-notes/floodfill/flood_fill.py
+++ lecture-notes/floodfill/flood_fill.py
@@ -18,25 +18,37 @@ def print_image():
         print("".join(line))
 
 
+# visual representation of the stack
+depth = 0
+
+
 def floodfill(row, col, c):
+    global depth
+
+    depth += 1
+    # this is just to watch the floodfilling live
+    system('clear')
+    print_image()
+    print(">" * depth)
+    time.sleep(0.25)
     # first check if row and col are in bounds
     if row < 0 or row > len(image) - 1 or col < 0 or col > len(image[0]) - 1:
+        depth -= 1
         return
         # base case to see if coordinate is already floodfilled
     if image[row][col] != ".":
+        depth -= 1
         return
-    else:
-        # fill the coordinate
-        image[row][col] = c
-        # watch it live!
-        system("clear")
-        print_image()
-        time.sleep(1)
-        # recursively try to fill each neighbor pixel
-        floodfill(row - 1, col, c)
-        floodfill(row+1, col, c)
-        floodfill(row, col+1, c)
-        floodfill(row, col-1, c)
+    # fill the coordinate
+    image[row][col] = c
+
+    # recursively try to fill each neighbor pixel
+    floodfill(row - 1, col, c)
+    floodfill(row+1, col, c)
+    floodfill(row, col+1, c)
+    floodfill(row, col-1, c)
+
+    depth -= 1
 
 
 floodfill(2, 5, '*')
